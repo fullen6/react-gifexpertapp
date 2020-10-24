cfcdd61337fe1fec721a92b8fe4f4254604a0b51
paul <nestor.cruz@munaysoft.com>
2020-10-24 11:24:47 -0500

    agrego docs

---
+++ docs/precache-manifest.5a0519a9bbeb36cbfb55c2e457a09dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449d513f08a9fd64cf8ca07151557d65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b785cc7f95d70c480a29",
+    "url": "/static/css/main.3f93df67.chunk.css"
+  },
+  {
+    "revision": "c891655c70870ca4f1e5",
+    "url": "/static/js/2.00eec8fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00eec8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b785cc7f95d70c480a29",
+    "url": "/static/js/main.89a93cf0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
